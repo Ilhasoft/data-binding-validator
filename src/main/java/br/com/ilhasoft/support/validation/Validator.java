256e9685217a7a338081562108857ce08600868a
John Cordeiro <john.dalton@ilhasoft.com.br>
2016-05-14 21:56:58 -0300

    Add new type validators and validation types

--- src/main/java/br/com/ilhasoft/support/validation/Validator.java
+++ src/main/java/br/com/ilhasoft/support/validation/Validator.java
@@ -8,17 +8,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import br.com.ilhasoft.support.validation.rule.EmailTypeRule;
 import br.com.ilhasoft.support.validation.rule.MaxLengthRule;
 import br.com.ilhasoft.support.validation.rule.MinLengthRule;
 import br.com.ilhasoft.support.validation.rule.Rule;
+import br.com.ilhasoft.support.validation.rule.UrlTypeRule;
 
 /**
  * Created by john-mac on 5/14/16.
  */
 public class Validator {
 
+    private static final int FIELD_VALIDATION_MODE = 0;
+    private static final int FORM_VALIDATION_MODE = 1;
+
     private ViewDataBinding target;
     private final List<Rule> rules;
+
+    private int mode = FIELD_VALIDATION_MODE;
 
     public Validator(ViewDataBinding target) {
         this.target = target;
@@ -29,6 +36,8 @@ public class Validator {
     private void setupRules() {
         this.rules.add(new MinLengthRule());
         this.rules.add(new MaxLengthRule());
+        this.rules.add(new EmailTypeRule());
+        this.rules.add(new UrlTypeRule());
     }
 
     public boolean validate() {
@@ -37,11 +46,23 @@ public class Validator {
         List<View> viewWithValidations = getViewsWithValidation();
         for (View viewWithValidation : viewWithValidations) {
             for (Rule rule : rules) {
-                valid = valid && rule.validate(viewWithValidation);
+                if(mode == FORM_VALIDATION_MODE) {
+                    valid = valid & rule.validate(viewWithValidation);
+                } else {
+                    valid = valid && rule.validate(viewWithValidation);
+                }
             }
         }
 
         return valid;
+    }
+
+    public void enableFormValidationMode() {
+        this.mode = FORM_VALIDATION_MODE;
+    }
+
+    public void enableFieldValidationMode() {
+        this.mode = FIELD_VALIDATION_MODE;
     }
 
     private List<View> getViewsWithValidation() {
