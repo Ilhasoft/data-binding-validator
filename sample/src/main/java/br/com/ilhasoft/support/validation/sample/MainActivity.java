55204beb035651cb58d6adfaf54bdc226e6cc14e
John Cordeiro <john.dalton@ilhasoft.com.br>
2016-05-26 15:39:56 -0300

    Add sample of library

--- sample/src/main/java/br/com/ilhasoft/support/validation/sample/MainActivity.java
+++ sample/src/main/java/br/com/ilhasoft/support/validation/sample/MainActivity.java
@@ -3,17 +3,43 @@ package br.com.ilhasoft.support.validation.sample;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import br.com.ilhasoft.support.validation.Validator;
 import br.com.ilhasoft.support.validation.sample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private static final String TAG = "MainActivity";
+
+    private Validator validator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.validate.setOnClickListener(onValidateClickListener);
+
+        validator = new Validator(binding);
+        validator.enableFormValidationMode();
+    }
+
+    private View.OnClickListener onValidateClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (validator.validate()) {
+                saveToDatabase();
+            } else {
+                Toast.makeText(MainActivity.this, "Dados inválidos!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    private void saveToDatabase() {
+        Log.i(TAG, "Salvar os dados no banco de dados");
     }
 
 }
