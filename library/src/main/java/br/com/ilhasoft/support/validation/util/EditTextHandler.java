a7c02fa05e7928793a73258a40cf11cb53371956
Daniel San <danielsan@ilhasoft.com.br>
2016-11-19 21:39:12 -0300

    Fixed implementation of disableErrorOnChanged
    Signed-off-by: Daniel San <danielsan@ilhasoft.com.br>

--- library/src/main/java/br/com/ilhasoft/support/validation/util/EditTextHandler.java
+++ library/src/main/java/br/com/ilhasoft/support/validation/util/EditTextHandler.java
@@ -1,9 +1,13 @@
 package br.com.ilhasoft.support.validation.util;
 
+import android.databinding.adapters.ListenerUtil;
 import android.support.design.widget.TextInputLayout;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.widget.TextView;
+
+import br.com.ilhasoft.support.validation.R;
 
 /**
  * Created by john-mac on 5/14/16.
@@ -11,29 +15,36 @@ import android.widget.TextView;
 public class EditTextHandler {
 
     public static void removeError(TextView textView) {
-        setError(textView, null);
+        EditTextHandler.setError(textView, null);
     }
 
     public static void setError(TextView textView, String errorMessage) {
-        if(textView.getParent() instanceof TextInputLayout) {
+        if (textView.getParent() instanceof TextInputLayout) {
             TextInputLayout textInputLayout = (TextInputLayout) textView.getParent();
+            textInputLayout.setErrorEnabled(!TextUtils.isEmpty(errorMessage));
             textInputLayout.setError(errorMessage);
         } else {
             textView.setError(errorMessage);
         }
     }
 
-    public static void disableErrorOnChanged(final TextView view) {
-        view.addTextChangedListener(new TextWatcher() {
+    public static void disableErrorOnChanged(final TextView textView) {
+        if (ListenerUtil.<TextWatcher>getListener(textView, R.id.text_watcher_clear_error) != null) {
+            return;
+        }
+
+        final TextWatcher textWatcher = new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                setError(view, null);
+                EditTextHandler.setError(textView, null);
             }
             @Override
-            public void afterTextChanged(Editable s) {}
-        });
+            public void afterTextChanged(Editable s) { }
+        };
+        textView.addTextChangedListener(textWatcher);
+        ListenerUtil.trackListener(textView, textView, R.id.text_watcher_clear_error);
     }
 
 }
