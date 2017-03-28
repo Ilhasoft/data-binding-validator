ef176bd2df58a241319634b09000c85ab63117eb
John Cordeiro <john.dalton@ilhasoft.com.br>
2017-03-28 09:56:10 -0300

    Update sample with speficic views' validations

--- sample/src/main/java/br/com/ilhasoft/support/validation/sample/MainActivity.java
+++ sample/src/main/java/br/com/ilhasoft/support/validation/sample/MainActivity.java
@@ -1,11 +1,14 @@
 package br.com.ilhasoft.support.validation.sample;
 
 import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import java.util.Arrays;
 
 import br.com.ilhasoft.support.validation.Validator;
 import br.com.ilhasoft.support.validation.sample.databinding.ActivityMainBinding;
@@ -14,20 +17,38 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    private ActivityMainBinding binding;
     private Validator validator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.validate.setOnClickListener(onValidateClickListener);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.validateName.setOnClickListener(onValidateNameClickListener);
+        binding.validateMultiple.setOnClickListener(onValidateMultipleClickListener);
+        binding.validate.setOnClickListener(onValidateAllClickListener);
 
         validator = new Validator(binding);
         validator.enableFormValidationMode();
     }
 
-    private View.OnClickListener onValidateClickListener = new View.OnClickListener() {
+    private View.OnClickListener onValidateNameClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            validator.validate(binding.name);
+        }
+    };
+
+    private View.OnClickListener onValidateMultipleClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            validator.validate(Arrays.asList(binding.username, binding.email));
+        }
+    };
+
+
+    private View.OnClickListener onValidateAllClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             if (validator.validate()) {
